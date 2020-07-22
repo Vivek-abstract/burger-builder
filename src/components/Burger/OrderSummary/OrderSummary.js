dcc8da1a8c183126b2b15b806c7969887e3aba97
Vivek Gawande <vivek040997@gmail.com>
2020-07-22 13:07:35 +0530

    Add navigation and logo

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button'
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+    .map(key=> {
+        return (
+            <li key={key}>
+                <span style={{textTransform: "capitalize"}}>{key}</span>: {props.ingredients[key]}
+            </li>
+        )
+    });
+
+    return (
+        <Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Total price: <strong>{props.totalPrice.toFixed(2)}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+    )
+}
+
+export default orderSummary;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from './Layout.module.css';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <div>Toolbar; Sidedrawer; Backdrop</div>
         <main className={classes.content}>{props.children}</main>
     </Aux>
